9d8a9ada89cacab495188e9c5b1afc22f0462bd4
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-10-04 07:38:16 +0100

    created routes and widcard routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/jobs', function () {
+    return view('jobs',
+    ['jobs' => [
+                    [
+                    'title' => 'Director',
+                    'salary' => 50000
+                    ],
+                    [
+                        'title' => 'Developer',
+                        'salary' => 100000
+                    ],
+                    [
+                        'title' => 'Teacher',
+                        'salary' => 5000
+                    ]
+            ]
+        ]
+    );
+});
